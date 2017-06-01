199eb29310d4849b790d1bc47b0a5e80f21dae03
Rodman Aguirre Méndez <rodfagui@gmail.com>
2017-06-01 17:40:29 -0500

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.3'
 # Use Puma as the app server
@@ -20,6 +19,11 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+gem 'faker', '1.7.3'
+
+gem 'will_paginate', '3.1.5'
+
+gem 'bootstrap-will_paginate', '1.0.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string
+#  admin           :boolean
 #
 
 class User < ApplicationRecord
@@ -16,7 +17,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
