9f6b0308e91fb6da367db6127666b405d7629d5a
Rodman Aguirre Méndez <rodfagui@gmail.com>
2017-06-01 13:22:05 -0500

    Add model user

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  email           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
+#
+
+class User < ApplicationRecord
+	before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 60 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
